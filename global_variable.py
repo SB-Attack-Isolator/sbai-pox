ad7a80b6a6a309df29f86be91b581b1cd30247be
XxxXxxXxx233 <lc18038037737@gmail.com>
2023-05-04 18:23:19 +0000

    bugfix: lock is none

--- global_variable.py
+++ global_variable.py
@@ -4,6 +4,8 @@ Author: Shenghua Chen
 Version: Aug 30th, 2023
 """
 
+import threading
+
 LOGGER_KEY = "logger"
 ATTACKER_KEY = "attacker"
 CONTROLLER_MESSAGE_QUEUE_KEY = "controller_message_queue"
@@ -15,6 +17,11 @@ def __init__():
     global _shared_var
     if not("_shared_var" in globals() and _shared_var is dict):
         _shared_var = {}
+
+    set_lock(LOGGER_KEY, threading.Lock())
+    set_lock(ATTACKER_KEY, threading.Lock())
+    set_lock(CONTROLLER_MESSAGE_QUEUE_KEY, threading.Lock())
+    
 
 def lock_var(key):
     """
@@ -113,12 +120,12 @@ def set_lock(key, lock):
 def set_var(key, value, useLock = False):
     global _shared_var
     # get old lock object
-    if useLock and key in _shared_var:
+    if key in _shared_var:
         _, lock = _shared_var[key]
     else:
         lock = None
     # Check if need to unlock
-    if lock is None:
+    if not useLock or lock is None:
         _shared_var[key] = (value, lock)
     else:
         lock.acquire()
